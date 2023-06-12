394b27ed44c6e5c2b83fbfa20ba7589618750309
Eric Hanchrow <eric.hanchrow@gmail.com>
2023-06-12 08:14:56 -0700

    Add some smoke tests to runme.sh

--- runme.sh
+++ runme.sh
@@ -10,4 +10,12 @@ then
    poetry install
 fi
 
-./.venv/bin/python3 transitbuddy_backend/flask_controller.py
+./.venv/bin/python3 transitbuddy_backend/flask_controller.py &
+
+sleep 1
+
+set -e
+# A couple of smoke tests
+curl --silent http://127.0.0.1:8000/stationlist | grep Astoria
+curl --silent http://127.0.0.1:8000/train/l
+wait
